45291a9be345039422eae3135b44f61a71e79e36
Simon Prickett <simon@redislabs.com>
2021-06-09 00:11:04 +0100

    2 scenes, and a do nothing backend.

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const path = require('path');
+const Redis = require('ioredis');
 
 const PORT = process.env.port||8080;
 
 const app = express();
+const redis = new Redis(); // TODO configurable location.
 
 // Serve the front end statically from the 'public' folder.
 app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/api/room/:roomNumber', (req, res) => {
+  res.json({ todo: true });
+})
 
 // Start the server.
 app.listen(PORT, () => {
