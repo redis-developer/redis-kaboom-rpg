add4a7aebe3f3db572afbc14d8c4d049dac448cf
Simon Prickett <simon@redislabs.com>
2021-06-09 00:02:19 +0100

    Two scene game.

--- public/static/js/game.js
+++ public/static/js/game.js
@@ -1,16 +1,31 @@
 window.onload = function () {
   const k = kaboom({
+    global: true,
     width: 640,
     height: 480,
+    clearColor: [0, 0, 0, 1],
     canvas: document.getElementById('game')
   });
 
-  k.scene('start', () => {
-    k.add([
-        k.text('well it\'s a start!', 32),
-        k.pos(20, 200),
+  scene('play', (roomNumber) => { 
+    add([
+      text(`todo: room ${roomNumber}!`, 28),
+      pos(width() / 2, height() / 2),
+      origin("center"),
     ]);
   });
 
-  k.start('start');
+  scene('start', () => {
+    add([
+      text("press space to begin!", 28),
+      pos(width() / 2, height() / 2),
+      origin("center"),
+    ]);
+
+    keyPress('space', () => {
+      go('play', 0);
+    });
+  });
+
+  start('start');
 };
