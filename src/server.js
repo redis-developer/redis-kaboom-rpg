4eda9567e27f17a6cfb30ad56bec865eebc4995e
Simon Prickett <simon@redislabs.com>
2021-06-09 14:10:25 +0100

    Made Redis location configurable.

--- src/server.js
+++ src/server.js
@@ -2,19 +2,27 @@ const express = require('express');
 const path = require('path');
 const Redis = require('ioredis');
 
-const PORT = process.env.port||8080;
+const PORT = process.env.PORT||8080;
+const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1'; 
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
 
 const app = express();
-const redis = new Redis(); // TODO configurable location.
+const redis = new Redis({
+  host: REDIS_HOST,
+  port: REDIS_PORT,
+  password: REDIS_PASSWORD 
+});
 
 // Serve the front end statically from the 'public' folder.
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Get details for a specified room number from Redis.
 app.get('/api/room/:roomNumber', (req, res) => {
   res.json({ todo: true });
 })
 
 // Start the server.
 app.listen(PORT, () => {
-  console.log(`Redis Kaboom RPG server listening on port ${PORT}.`);
+  console.log(`Redis Kaboom RPG server listening on port ${PORT}, Redis at ${REDIS_HOST}:${REDIS_PORT}.`);
 });
