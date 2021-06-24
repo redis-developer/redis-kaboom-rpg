2d11fc387a96fedbb7ac25c2a016e7140a1d55bf
Simon Prickett <simon@redislabs.com>
2021-06-24 19:40:36 +0100

    Adds player, initial hard coded map, and key bindings.

--- public/static/js/game.js
+++ public/static/js/game.js
@@ -1,23 +1,73 @@
 window.onload = function () {
   const k = kaboom({
     global: true,
-    width: 640,
-    height: 480,
+    scale: 4,    
     clearColor: [0, 0, 0, 1],
-    canvas: document.getElementById('game')
+    canvas: document.getElementById('game'),
+    width: 150,
+    height: 150
   });
 
+  loadRoot('/');
+  loadSprite('player', 'sprites/player.png');
+
   scene('play', (roomNumber) => { 
+    const levelMap = [
+      '           ',
+      '           ',
+      '           ',
+      '           ',
+      '           ',
+      '           ',
+      '           ',
+      '           ',
+      '           ',
+      '           ',
+      '     @     ',
+    ];
+
+    addLevel(levelMap, {
+      width: 11,
+      height: 11,
+      pos: vec2(20, 20),
+      "@": [
+        sprite('player'),
+        'player'
+      ]
+    });
+
+    const player = get('player')[0];
+
+    const directions = {
+      'left': vec2(-1, 0),
+      'right': vec2(1, 0),
+      'up': vec2(0, -1),
+      'down': vec2(0, 1)
+    };
+
+    for (const direction in directions) {
+		  keyPress(direction, () => {
+		  });
+		  keyDown(direction, () => {
+        console.log(direction);
+			  //player.move(dirs[dir].scale(60));
+		  });
+	  }
+
     add([
-      text(`todo: room ${roomNumber}!`, 28),
+      text(`todo: room ${roomNumber}!`, 6),
       pos(width() / 2, height() / 2),
       origin("center"),
     ]);
+
+    player.action(() => {
+      player.resolve();
+    });
   });
 
   scene('start', () => {
     add([
-      text("press space to begin!", 28),
+      text("press space to begin!", 6),
       pos(width() / 2, height() / 2),
       origin("center"),
     ]);
