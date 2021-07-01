44d5b396e8d836ef22df5f1c14ffaf278076ac78
Simon Prickett <simon@redislabs.com>
2021-07-01 15:02:25 +0100

    Commented the data loader.

--- src/data_loader.js
+++ src/data_loader.js
@@ -5,16 +5,21 @@ const REDIS_PORT = process.env.REDIS_PORT || 6379;
 const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
 
 const loadGameData = async () => {
+  // Connect to Redis...
   const redis = new Redis({
     host: REDIS_HOST,
     port: REDIS_PORT,
     password: REDIS_PASSWORD
   });
 
+  // Where we'll store the room data in Redis.
   const GAME_MAP_KEY = 'kaboom:rooms';
 
+  // Load the room data from JSON file.
   const gameData = require('../game_map.json');
 
+  // Delete any previous data in Redis and store the room data
+  // as a JSON document.
   await redis.del(GAME_MAP_KEY);
   await redis.call('JSON.SET', GAME_MAP_KEY, '.', JSON.stringify(gameData));
 
