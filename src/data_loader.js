eab06e215bbec4636a089899b0136b71f4ae7c03
tragu <tankasalarahulgupta@gmail.com>
2021-10-11 19:28:56 +0530

    support for all platforms

--- src/data_loader.js
+++ src/data_loader.js
@@ -1,6 +1,6 @@
 const Redis = require('ioredis');
 
-const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1';
+const REDIS_HOST = process.env.REDIS_HOST || 'redis';
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
 const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
 
